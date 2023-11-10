b71f213306dcf7cfe935248bd3904f2ecf593398
Igor Feoktistov <feoktist92@gmail.com>
2023-11-11 00:22:18 +0800

    changes config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack(config: any) {
+    webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
             use: ['@svgr/webpack'],
